f268628c90772dc7687f2b2a28c86f24455805e0
GKM090503 <72422431+NormalHuman-Anything@users.noreply.github.com>
2022-07-23 11:56:09 +0900

    버튼으로 저항 알아보기

--- button/button.ino
+++ button/button.ino
@@ -1,9 +1,16 @@
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(8, INPUT_PULLUP);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+ if (digitalRead(8) == LOW) {
+  Serial.println("0");
 
+ }else if (digitalRead(8) == HIGH) {
+  Serial.println("1");
+
+ }
 }
