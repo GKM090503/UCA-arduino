3da9ca13417cae0328a628a2bcb793725b050fca
GKM090503 <72422431+NormalHuman-Anything@users.noreply.github.com>
2022-07-23 12:49:50 +0900

    UCA

--- button/button.ino
+++ button/button.ino
@@ -1,16 +1,21 @@
+#include<DHT.h>
+
+#define DHTPIN A1
+#define DHTTYPE dht11
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(8, INPUT_PULLUP);
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
- if (digitalRead(8) == LOW) {
-  Serial.println("0");
+  int h = dht.readHumidity();
+  int t = dht.readTemperature();
 
- }else if (digitalRead(8) == HIGH) {
-  Serial.println("1");
+  Serial print("Humidity : ");
+  Serial println(h);
+  Serial print("Temperature : ");
+  Serial print(t);
 
- }
+  delay(1000);
 }
